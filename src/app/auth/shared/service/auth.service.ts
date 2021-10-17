eab7e7677385460a2e66c661f63c9be37460edfb
reza shakeri <r.shakeri9210@gmail.com>
2021-10-18 00:46:46 +0330

    [feat] minor auth changes

--- src/app/auth/shared/service/auth.service.ts
+++ src/app/auth/shared/service/auth.service.ts
@@ -1,17 +1,14 @@
-import { HttpClient } from "@angular/common/http";
 import { Injectable } from "@angular/core";
-import { GlobalService } from "src/app/core/services/global.service";
-import { ApiRequest } from "src/app/core/services/request.service";
 import { environment } from "src/environments/environment";
 
 @Injectable({
   providedIn: "root",
 })
 export class AuthService {
-  constructor(private http: HttpClient) {}
+  constructor() {}
 
   login(model) {
     let url = environment.serviceBaseUrl;
-    return this.http.post(`${url}login`, model);
+    // return this.http.post(`${url}login`, model);
   }
 }
