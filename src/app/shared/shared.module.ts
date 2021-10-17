dfd93d3426614418fc6dd1bf41f4b1a511c89174
reza shakeri <r.shakeri9210@gmail.com>
2021-10-18 00:46:05 +0330

    [wip] remove extras

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -3,10 +3,8 @@ import { NgModule } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { MatTableModule } from '@angular/material/table';
-import { ExtractObjectPipe } from './Pipe/extract-object.pipe';
-import { ReplaceUnderlinePipe } from './Pipe/replace-underline.pipe';
 
-const PIPES = [ExtractObjectPipe, ReplaceUnderlinePipe];
+const PIPES = [];
 const matModules = [MatIconModule, MatButtonModule, MatTableModule];
 @NgModule({
   declarations: [...PIPES],
